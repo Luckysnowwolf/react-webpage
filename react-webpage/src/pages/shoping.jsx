4b6afdd253c1cacd5a56f6b28393a833aa45b419
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-12-11 10:40:08 +0100

    updates shoping.jsx

--- react-webpage/src/pages/shoping.jsx
+++ react-webpage/src/pages/shoping.jsx
@@ -1,55 +1,55 @@
 import "./page.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function shoping() {
     const [Poster, setPoster] = useState(["wolfposter", "leopardposter", "foxposter", "huskyposter", "catposter"]);
     const [newTask, setNewTask] = useState([])
 
 
-    function handleAddPoster(index) {
-        
-        
+    function handleaddPoster() {
+        setPoster([...Poster, inputValue])
+        setInputValue("");
+
     }
     function handleRemovePoster(index) {
         setPoster(Poster.filter((_, i) => i !== index));
     }
 
 
-function handleAmountOf() {
+    function handleAmountOf() {
         let count = 0;
         const handleEvent = (number) => {
             if (count < 3) {
                 count++
             } else {
             }
-        }}
-        const [number, setNumber] = useState(0);
-        const increseNumber = () => {
-            setNumber(number + 1);
         }
+    }
+    const [number, setNumber] = useState(0);
+    const increseNumber = () => {
+        setNumber(number + 1);
+    }
     return (
-        <div class="shopingSection">
-            <h2 class="Header"> list of posters</h2>
-            <ul class="options">
+        <div className="shopingSection">
+            <h2 className="Header"> list of posters</h2>
+            <ul className="options">
                 {Poster.map((Poster, index) => (
                     <li key={index} onClick={() => handleRemovePoster(index)}>{Poster}</li>
                 ))}
             </ul>
-            <ul class="options">
-                {Poster.map((Poster, index) => (
-                    <li key={index} onClick={() => handleRemovePoster(index)}>{Poster}</li>
-                ))}
-            </ul>
-            <ul class="options">
-                {Poster.map(newTask => (
-                    <li key={newTask.id}>{newTask.Poster}</li>
+            <ul className="options">
+                {newTask.map(poster => (
+                    <li key={poster.id}>{poster.Poster}</li>
                 ))}
             </ul>
             <input
-             type="text" class="textzone" placeholder="enter poster animal..." id="posterOption"
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}/>
-            <button class="add" onClick={() => handleAddPoster([...newTask, {id: nextId++, Poster: name }])}>add poster</button><button class="amount" onClick={handleAmountOf}>amount: {number}</button><button class="plus" onClick={increseNumber}>+</button><button class="remove" onClick={() => handleRemovePoster(index)}>remove poster</button>
+                type="text" className="textzone"
+                placeholder="enter poster animal..." id="posterOption"
+                value={newTask}
+                onChange={(e) => setPoster(e.target.value)} />
+            <button className="add"
+                onClick={() => {setNewTask([...newTask, { id: nextId++, name: Poster }])}}>add poster</button>
+            <button className="amount" onClick={handleAmountOf}>amount: {number}</button><button className="plus" onClick={increseNumber}>+</button><button className="remove" onClick={() => handleRemovePoster(index)}>remove poster</button>
         </div>
     );
 }
